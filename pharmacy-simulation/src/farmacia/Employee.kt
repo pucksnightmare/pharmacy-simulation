72cc4cf7d0abe27ceba6abfd876bf1989971e6d2
ElPunch <alfonsorobledo3548@gmail.com>
2025-11-12 15:43:32 -0600

    Refactor Employee class to implement Runnable and define work method for subclasses

--- pharmacy-simulation/src/farmacia/Employee.kt
+++ pharmacy-simulation/src/farmacia/Employee.kt
@@ -1,4 +1,18 @@
 package farmacia
 
-class Employee {
+open class Employee(val employeeName: String) : Runnable {
+
+    // Método común que las subclases deben redefinir (polimorfismo)
+    open fun work() {
+        println("$employeeName está trabajando.")
+    }
+
+    // run() llamado cuando el Thread arranca
+    override fun run() {
+        try {
+            work()
+        } catch (e: InterruptedException) {
+            println("$employeeName fue interrumpido.")
+        }
+    }
 }

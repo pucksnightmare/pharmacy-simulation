ad4f02b5982a72bf678b1a64be6b05cdb11648cf
ElPunch <alfonsorobledo3548@gmail.com>
2025-11-12 15:44:40 -0600

    Refactor Pharmacist class to extend Employee and implement work method for prescription preparation

--- pharmacy-simulation/src/farmacia/Pharmacist.kt
+++ pharmacy-simulation/src/farmacia/Pharmacist.kt
@@ -1,4 +1,18 @@
 package farmacia
 
-class Pharmacist {
+class Pharmacist(name: String) : Employee(name) {
+
+    override fun work() {
+        val prescriptions = listOf("hipertensión", "diabetes", "alergias", "migraña")
+        try {
+            for (i in 1..4) {
+                val recipe = prescriptions.random()
+                println("${employeeName} está preparando una receta para $recipe. Etiquetando y verificando dosis...")
+                Thread.sleep(700)
+            }
+            println("${employeeName} ha terminado su turno como farmacéutico.")
+        } catch (e: InterruptedException) {
+            println("${employeeName} interrumpido mientras preparaba recetas.")
+        }
+    }
 }
